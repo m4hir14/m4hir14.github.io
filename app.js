b83b56b6552d263448af6c9bdb5601c723f88840
m4hir14 <mahiraghada@gmail.com>
2024-09-10 11:43:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const images = document.querySelectorAll("img");
+    
+    for(const image of images){
+        fetch("https://foodish-api.com/images/pizza/pizza61.jpg")
+        .then(response => response.json())
+        .then(data => {
+            image.src = data.message
+            image.width = 100;
+            image.height = 100;
+        })
+    }
+    
+})
