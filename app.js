c02421dfe8eaba202d56d632195053766894909a
m4hir14 <mahiraghada@gmail.com>
2024-09-10 11:25:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
     
     for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://foodish-api.com/images/pizza/pizza61.jpg")
         .then(response => response.json())
         .then(data => {
             image.src = data.message
